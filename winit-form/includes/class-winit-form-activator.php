2fd0ce52c561c9381054eacb1fc0073c32531055
DanieGalue <daniela.galue10@gmail.com>
2023-01-19 17:02:23 -0400

    Add files via upload

---
+++ winit-form/includes/class-winit-form-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://delcosgroup.com
+ * @since      1.0.0
+ *
+ * @package    Winit_Form
+ * @subpackage Winit_Form/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Winit_Form
+ * @subpackage Winit_Form/includes
+ * @author     D.G. <delcosgroup@gmail.com>
+ */
+class Winit_Form_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ winit-form/includes/class-winit-form-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://delcosgroup.com
+ * @since      1.0.0
+ *
+ * @package    Winit_Form
+ * @subpackage Winit_Form/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Winit_Form
+ * @subpackage Winit_Form/includes
+ * @author     D.G. <delcosgroup@gmail.com>
+ */
+class Winit_Form_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
